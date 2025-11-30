8cb758dea8c06423937ef33bb016b2d9769d09ce
luzdrik <secundariayume@gmail.com>
2025-11-30 02:17:21 -0300

    Tela de carregameto implementada

--- src/main/java/aplicacao/App.java
+++ src/main/java/aplicacao/App.java
@@ -1,14 +1,16 @@
 package aplicacao;
 
+import java.io.IOException;
+import javafx.application.Platform;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-
+import javafx.stage.StageStyle;
 
 public class App extends Application {
 
@@ -22,30 +24,56 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        scene = new Scene(loadFXML("main"), 640, 480);
+        // Tela de Splash
+        Image logoImage = new Image(getClass().getResource("/imagens/logo.png").toExternalForm());
+        ImageView logoView = new ImageView(logoImage);
+        logoView.setFitWidth(300);
+        logoView.setFitHeight(300);
+        
+        StackPane splashRoot = new StackPane(logoView);
+        Stage splashStage = new Stage(StageStyle.UNDECORATED);
+        Scene splashScene = new Scene(splashRoot);
+        splashStage.setScene(splashScene);
+        splashStage.show();
 
-        scene.getStylesheets().add(
-            App.class.getResource("/styles.css").toExternalForm()
-        );
+        // Thread para simular a tela de "carregamento"
+        new Thread(() -> {
+            try {
+                Thread.sleep(2000);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
-        stage.setTitle("Pomodoro Timer!");
-        stage.setResizable(false);
-        stage.setScene(scene);
-        stage.show();
+            javafx.application.Platform.runLater(() -> {
+                try {
+                    splashStage.close();
 
-        // Ícone da API <3
-        Image icon = new Image(App.class.getResource("/imagens/icone.png").toExternalForm());
-        stage.getIcons().add(icon);
+                    scene = new Scene(loadFXML("main"), 640, 480);
+                    scene.getStylesheets().add(App.class.getResource("/styles.css").toExternalForm());
 
-        stage.setScene(scene);
-        stage.show();
+                    stage.setTitle("Pomodoro Timer!");
+                    stage.setResizable(false);
+                    stage.setScene(scene);
+
+                    // Ícone da API <3
+                    Image icon = new Image(App.class.getResource("/imagens/icone.png").toExternalForm());
+                    stage.getIcons().add(icon);
+
+                    stage.show();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }).start();
     }
 
     static Object setRoot(String fxml) throws IOException {
-    FXMLLoader loader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-    Parent root = loader.load();
-    scene.setRoot(root);
-    return loader.getController();
+        FXMLLoader loader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        Parent root = loader.load();
+        scene.setRoot(root);
+        return loader.getController();
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
